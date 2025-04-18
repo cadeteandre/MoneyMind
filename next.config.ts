2eb196cdcf364a3dec3c83a258e85cb3162f6d84
Andre Cadete <andrecadete70@gmail.com>
2025-04-18 15:16:01 +0200

    setting up project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+} as unknown as NextConfig;
 
 export default nextConfig;
