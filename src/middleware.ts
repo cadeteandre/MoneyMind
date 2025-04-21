36e644b64d6295b135e10f7ab43214ff69634fa4
Andre Cadete <andrecadete70@gmail.com>
2025-04-21 21:20:35 +0200

    feat: creating and protecting sign in page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  // other public routes
+]);
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
+export default clerkMiddleware(async (auth, req) => {
+  if (isPublicRoute(req)) {
+    return; // Not required to authenticate
+  }
+  
+  // Protect routes that require authentication
+  const { userId } = await auth();
+  if (!userId) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+});
