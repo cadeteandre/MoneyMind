688423c9acf4c1db10dd910eaac1a06ee11b5101
Andre Cadete <andrecadete70@gmail.com>
2025-04-22 12:46:40 +0200

    feat: settin up transaction form with shadcn components (only UI + show the submited content on the console)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { TransactionForm } from "@/components/TransactionForm";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -12,6 +13,9 @@ export default async function DashboardPage() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold mb-4">Welcome to your Dashboard 👋</h1>
       <p className="text-muted-foreground">User ID: {userId}</p>
+      <div>
+        <TransactionForm />
+      </div>
     </div>
   );
 }
